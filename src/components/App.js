652ad29e4bb7d5413ca520e47cb8664e1cfc80ae
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-15 20:36:00 +0530

    Assignment solve

--- src/components/App.js
+++ src/components/App.js
@@ -22,36 +22,29 @@
 //   )
 // }
 
-import React, {useReducer} from 'react'
+import React from 'react'
 import '../styles/App.css';
 const App = () => {
-  const initialState = 1;
 
-  const counterReducer = (state, action) => {
-    switch(action){
-      case "increment": return state + 1 ;
-      case "decrement": return state - 1 ;
-      default:
-        return 0;
-    }
-  };
-  
-  const[state, disptch] = useReducer(counterReducer, initialState);
+  const handleInput = (event) =>{
+    console.log('Input in #' + event.target.id + ' is ' + event.target.value);
+  }
 
+  // do not change id of input elements
   return (
     <div id="main">
-      <button id="increment" onClick={() => disptch("increment")}>Increment</button><br /> <br />
-      {
-        (state % 15 === 0 && (<div id="counter" className="fizzbuzz">{state}</div>)) ||
-        (state % 5 === 0 && (<div id="counter" className="buzz">{state}</div>)) ||
-        (state % 3 === 0 && (<div id="counter" className="fizz">{state}</div>)) ||
-        (state % 3 !== 0 && state % 5 !== 0 && state % 15 !== 0 &&
-          (<div id="counter" className="normal">{state}</div>)) 
-      }
-      <br />
-      <button id="decrement" onClick={() => disptch("decrement")}>Decrement</button><br /> <br />
+      <label htmlFor='text-input'>Text Input:- </label>
+      <input onChange={handleInput} id="text-input" type={'text'} />
+
+      <br/>
+      <br/>
+
+      <label htmlFor='num-input'>Number input</label>
+      <input onChange={handleInput} id="num-input"  type={'number'} />
+      <br/>
     </div>
   )
 }
+
 
 export default App;
