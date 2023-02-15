2e51b4b3e8065bd27d2d8cbe7c4bee4ac61a5126
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-15 20:32:22 +0530

    Assignment solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,57 @@
-import React from 'react'
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
+
+//   const handleInput = (event) =>{
+//    // use console.log
+//   }
+
+//   // do not change id of input elements
+//   return (
+//     <div id="main">
+//       <label htmlFor='text-input'>Text Input:- </label>
+//       <input id="text-input" type={'text'} />
+
+//       <br/>
+//       <br/>
+
+//       <label htmlFor='num-input'>Number input</label>
+//       <input id="num-input"  type={'number'} />
+//       <br/>
+//     </div>
+//   )
+// }
+
+import React, {useReducer} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const initialState = 1;
 
-  const handleInput = (event) =>{
-   // use console.log
-  }
+  const counterReducer = (state, action) => {
+    switch(action){
+      case "increment": return state + 1 ;
+      case "decrement": return state - 1 ;
+      default:
+        return 0;
+    }
+  };
+  
+  const[state, disptch] = useReducer(counterReducer, initialState);
 
-  // do not change id of input elements
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
-
-      <br/>
-      <br/>
-
-      <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
-      <br/>
+      <button id="increment" onClick={() => disptch("increment")}>Increment</button><br /> <br />
+      {
+        (state % 15 === 0 && (<div id="counter" className="fizzbuzz">{state}</div>)) ||
+        (state % 5 === 0 && (<div id="counter" className="buzz">{state}</div>)) ||
+        (state % 3 === 0 && (<div id="counter" className="fizz">{state}</div>)) ||
+        (state % 3 !== 0 && state % 5 !== 0 && state % 15 !== 0 &&
+          (<div id="counter" className="normal">{state}</div>)) 
+      }
+      <br />
+      <button id="decrement" onClick={() => disptch("decrement")}>Decrement</button><br /> <br />
     </div>
   )
 }
-
 
 export default App;
